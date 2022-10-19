2acff4bfc954c1a79e4f2067c858e554510221f3
ryan kane <ryan.kane12@gmail.com>
2022-10-19 10:58:34 -0700

    final

---
+++ main.py
@@ -0,0 +1,21 @@
+from distutils.log import error
+from turtle import fillcolor
+import qrcode
+
+
+def generate_qrCode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+
+URL = input("Enter your URL: ")
+generate_qrCode(URL)
